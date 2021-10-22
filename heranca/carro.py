cb38df6ce815588521a248109ec421cec5866320
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2021-10-22 10:40:49 -0300

    Update carro.py

--- heranca/carro.py
+++ heranca/carro.py
@@ -1,12 +1,13 @@
 import veiculo
 import locale
+import pytz
 from datetime import datetime
 
 class Carro(veiculo.Veiculo):
 
   def Interacao(self):
     locale.setlocale(locale.LC_ALL, '')
-    hora_atual = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+    hora_atual = datetime.now(pytz.timezone('America/Sao_Paulo')).strftime('%d-%m-%Y %H:%M:%S')
     valor_formatado = locale.currency(self.getValor(), symbol=True, grouping=True, international=False)
     print(f"Marca: {self.getMarca()}")
     print(f"Modelo: {self.getModelo()}")
