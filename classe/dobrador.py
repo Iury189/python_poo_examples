0c95bc092550f887932cf093e5040acf46f906fd
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2021-10-23 12:13:05 -0300

    Update dobrador.py

--- classe/dobrador.py
+++ classe/dobrador.py
@@ -6,15 +6,11 @@ class Dobrador:
       self.elemento = elemento
     '''
     # Getters
-    def getNome(self):
-      return self.nome
-    def getElemento(self):
-      return self.elemento
+    def getNome(self): return self.nome
+    def getElemento(self): return self.elemento
     # Setters
-    def setNome(self, nome):
-      self.nome = nome
-    def setElemento(self, elemento):
-      self.elemento = elemento
+    def setNome(self, nome): self.nome = nome
+    def setElemento(self, elemento): self.elemento = elemento
     # Método Falar()
     def Falar(self):
       print(f"Meu nome é {self.getNome()} e o elemento que domino é o {self.getElemento()}.");
