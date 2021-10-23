c956efb6c5bb57100f81061a4aecbcef073acead
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2021-10-23 12:44:58 -0300

    Update main.py

--- classe/main.py
+++ classe/main.py
@@ -1,8 +1,8 @@
 from dobrador import Dobrador
 
-d1 = Dobrador();
+d1 = Dobrador()
 nome_dobrador = str(input("Digite o nome do dobrador: "))
-d1.setNome(nome_dobrador);
+d1.setNome(nome_dobrador)
 elemento_dobrador = str(input("Digite o elemento do dobrador: "))
-d1.setElemento(elemento_dobrador);
-d1.Falar();
+d1.setElemento(elemento_dobrador)
+d1.Falar()
