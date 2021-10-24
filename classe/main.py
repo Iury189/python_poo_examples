15a34dcef47e8aa34278fe51d505accef83eaab8
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2021-10-24 18:42:57 -0300

    Update main.py

--- classe/main.py
+++ classe/main.py
@@ -1,8 +1,18 @@
 from dobrador import Dobrador
 
 d1 = Dobrador()
-nome_dobrador = str(input("Digite o nome do dobrador: "))
-d1.setNome(nome_dobrador)
-elemento_dobrador = str(input("Digite o elemento do dobrador: "))
-d1.setElemento(elemento_dobrador)
+while True:
+  nome_dobrador = str(input("Digite o nome do dobrador: "))
+  d1.setNome(nome_dobrador);
+  if (len(nome_dobrador) == 0):
+    print('O nome não pode ficar vazio, digite novamente.')
+  else:
+    break
+while True:
+  elemento_dobrador = str(input("Digite o elemento do dobrador: "))
+  d1.setElemento(elemento_dobrador);
+  if (len(elemento_dobrador) == 0):
+    print('O elemento do dobrador não pode ficar vazio, digite novamente.')
+  else:
+    break
 d1.Falar()
