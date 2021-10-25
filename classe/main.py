22188b7f4a7f85f569bb716594347629f2816a07
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2021-10-25 19:50:28 -0300

    Update main.py

--- classe/main.py
+++ classe/main.py
@@ -6,13 +6,13 @@ while True:
   if (len(nome_dobrador) == 0):
     print('O nome não pode ficar vazio, digite novamente.')
   else:
-    d1.setNome(nome_dobrador)
+    d1.setNome(nome_dobrador.title())
     break
 while True:
   elemento_dobrador = str(input("Digite o elemento do dobrador: "))
   if (len(elemento_dobrador) == 0):
     print('O elemento do dobrador não pode ficar vazio, digite novamente.')
   else:
-    d1.setElemento(elemento_dobrador)
+    d1.setElemento(elemento_dobrador.title())
     break
 d1.Falar()
