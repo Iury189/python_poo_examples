b8355165ca042257d021250c568d9c64f5a56d86
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2021-10-27 07:42:17 -0300

    Update dobrador.py

--- classe/dobrador.py
+++ classe/dobrador.py
@@ -11,5 +11,3 @@ class Dobrador:
     # Setters
     def setNome(self, nome): self.nome = nome
     def setElemento(self, elemento): self.elemento = elemento
-    # Método Falar()
-    def Falar(self): print(f"Dobrador: {self.getNome()} | Elemento: {self.getElemento()}")
