e6fad7fc20c22746e22a342346364b1275dceabe
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2021-10-30 10:08:33 -0300

    Update dobrador.py

--- classe/dobrador.py
+++ classe/dobrador.py
@@ -5,9 +5,17 @@ class Dobrador:
       self.nome = nome
       self.elemento = elemento
     '''
+    '''
+    # Desconstrutor
+    def __del__(self):
+      print (f"Objeto {self.getNome()} foi excluído da memória.")
+    '''
     # Getters
     def getNome(self): return self.nome
     def getElemento(self): return self.elemento
     # Setters
     def setNome(self, nome): self.nome = nome
     def setElemento(self, elemento): self.elemento = elemento
+    # Método Falar()
+    def Falar(self):
+      print(f"Dobrador: {self.getNome()} | Elemento: {self.getElemento()}")
