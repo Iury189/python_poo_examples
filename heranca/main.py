6305b8d1580611a2420a9614f1abd872f9707577
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2022-01-08 19:19:38 -0300

    Update main.py

--- heranca/main.py
+++ heranca/main.py
@@ -1,15 +1,51 @@
 from carro import Carro
 
 c1 = Carro()
-marca_carro = str(input("Digite a marca do carro: "))
-c1.setMarca(marca_carro)
-modelo_carro = str(input("Digite o modelo do carro: "))
-c1.setModelo(modelo_carro)
-cor_carro = str(input("Digite a cor do carro: "))
-c1.setCor(cor_carro)
-ano_carro = int(input("Digite o ano de fabricação do carro: "))
-c1.setAno(ano_carro)
-valor_carro = float(input("Digite o valor do carro: "))
-c1.setValor(valor_carro)
+# Campo marca
+while True:
+  marca_carro = str(input("Digite a marca do carro: "))
+  if (len(marca_carro.split()) <= 0):
+    print('A marca do carro não pode ficar vazio, digite novamente.')
+  else:
+    c1.setMarca(marca_carro.title())
+    break
+# Campo modelo
+while True:
+  modelo_carro = str(input("Digite o modelo do carro: "))
+  if (len(modelo_carro.split()) <= 0):
+    print('O modelo do carro não pode ficar vazio, digite novamente.')
+  else:
+    c1.setModelo(modelo_carro.title())
+    break
+# Campo cor
+while True:
+  cor_carro = str(input("Digite a cor do carro: "))
+  if (len(cor_carro.split()) <= 0):
+    print('A cor do carro não pode ficar vazio, digite novamente.')
+  else:
+    c1.setCor(cor_carro.title())
+    break
+# Campo ano de fabricação
+while True:
+    try:
+        ano_carro = int(input("Digite o ano de fabricação do carro: "))
+        if (ano_carro <= 0):
+          print('O ano de fabricação do carro não pode ser menor ou igual a zero.')
+        else:
+          c1.setAno(ano_carro)
+          break
+    except Exception:
+        print("Apenas números inteiros são permitidos")
+# Campo valor do carro
+while True:
+    try:
+        valor_carro = float(input("Digite o valor do carro: "))
+        if (valor_carro <= 0):
+          print('O valor do carro não pode ser menor ou igual a zero.')
+        else:
+          c1.setValor(valor_carro)
+          break
+    except Exception:
+        print("Apenas números são permitidos")
 print("\n")
 c1.Interacao()
