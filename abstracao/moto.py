2545b5b288c99b6b3ea0a51c0a6ed0f54a19d243
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2022-01-08 19:30:31 -0300

    Update moto.py

--- abstracao/moto.py
+++ abstracao/moto.py
@@ -6,7 +6,7 @@ from datetime import datetime
 class Moto(veiculo.Veiculo):
   def Interacao(self):
     locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
-    hora_atual = datetime.now(pytz.timezone('America/Sao_Paulo')).strftime('%d-%m-%Y %H:%M:%S')
+    hora_atual = datetime.now(pytz.timezone('America/Sao_Paulo')).strftime('%d/%m/%Y %H:%M:%S')
     valor_formatado = locale.currency(self.getValor(), symbol = True, grouping = True, international = False)
     print(f"Marca: {self.getMarca()}")
     print(f"Modelo: {self.getModelo()}")
