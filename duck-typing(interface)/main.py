05cf14c6a1d365829e0721c62370de515774bd2e
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2022-03-02 11:18:28 -0300

    Update main.py

--- duck-typing(interface)/main.py
+++ duck-typing(interface)/main.py
@@ -2,8 +2,8 @@ from onca import Onca
 from puma import Puma
 from pantera import Pantera
 
-onca1 = Onca()
-puma1 = Puma()
-pantera1 = Pantera()
+onca = Onca()
+puma = Puma()
+pantera = Pantera()
 
-pantera1.Pegar(puma1)
+pantera.Pegar(puma)
