11cd1dcd5dbc8f2c2587d55df29749291fdfc08f
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2022-04-20 10:11:20 -0300

    Update main.py

--- abstracao/main.py
+++ abstracao/main.py
@@ -3,21 +3,21 @@ from moto import Moto
 m1 = Moto()
 while True:
     marca_moto = str(input("Digite a marca da moto: "))
-    if (len(marca_moto.split()) <= 0):
+    if marca_moto.strip() == '':
         print('A marca da moto não pode ficar vazia, digite novamente.')
     else:
         m1.setMarca(marca_moto.title())
         break
 while True:
     modelo_moto = str(input("Digite o modelo da moto: "))
-    if (len(modelo_moto.split()) <= 0):
+    if modelo_moto.strip() == '':
         print('O modelo da moto não pode ficar vazio, digite novamente.')
     else:
         m1.setModelo(modelo_moto.title())
         break
 while True:
     cor_moto = str(input("Digite a cor da moto: "))
-    if (len(cor_moto.split()) <= 0):
+    if cor_moto.strip() == '':
         print('A cor da moto não pode ficar vazia, digite novamente.')
     else:
         m1.setCor(cor_moto.title())
@@ -25,7 +25,7 @@ while True:
 while True:
     try:
         ano_moto = int(input("Digite o ano de fabricação da moto: "))
-        if (ano_moto <= 0):
+        if ano_moto <= 0:
           print('O ano de fabricação da moto não pode ser menor ou igual a zero.')
         else:
           m1.setAno(ano_moto)
@@ -35,7 +35,7 @@ while True:
 while True:
     try:
         valor_moto = float(input("Digite o valor da moto: "))
-        if (valor_moto <= 0):
+        if valor_moto <= 0:
           print('O valor da moto não pode ser menor ou igual a zero.')
         else:
           m1.setValor(valor_moto)
