0f00c9481952d9d2aff858c4f831009ea51d0cb6
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2022-08-02 09:48:14 -0300

    Update puma.py

--- duck-typing(interface)/puma.py
+++ duck-typing(interface)/puma.py
@@ -1,5 +1,5 @@
 class Puma:
-  def Andar(self):
-    print("Andando como puma")
-  def Falar(self):
-    print("Falando como puma")
+  def andar(self):
+    print("Andando como puma.")
+  def falar(self):
+    print("Falando como puma.")
